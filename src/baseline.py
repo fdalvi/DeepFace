d103bd6d45e25261bf57e6f6c72e8a2d270de96e
Fahim Imaduddin Dalvi <dalvifahim@gmail.com>
2016-02-18 19:40:48 -0800

    Fruitless changes

--- src/baseline.py
+++ src/baseline.py
@@ -6,10 +6,15 @@ import util
 
 DATA_PATH = '../data/dev_set/images_cropped/'
 
+
 def main():
+	caffe.set_mode_gpu()
+
 	images = ['Barack_Obama_441', 'Ali_Landry_78', 'Ben_Stiller_69']
-	data = util.get_attributes('../data/pubfig_attributes.txt', images)
-	print data.shape
+	images_with_path = [DATA_PATH + i + '.jpg' for i in images]
+
+	labels = util.get_attributes('../data/pubfig_attributes.txt', images)
+	print labels.shape
 
 	weights_path = 'vgg_face_caffe/VGG_FACE.caffemodel'
 	assert os.path.exists(weights_path)
@@ -17,11 +22,13 @@ def main():
 	solver_path = 'DeepFaceNet.prototxt'
 	assert os.path.exists(solver_path)
 
+
 	# caffe.set_mode_cpu()
 	net = caffe.Net(solver_path,
 	                weights_path,
 	                caffe.TEST)
-	solver = caffe.SGDSolver('solver.prototxt')
+	# solver = caffe.SGDSolver('solver.prototxt')
+	# solver.net.copy_from(weights_path)
 
 	transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
 	transformer.set_transpose('data', (2,0,1))
@@ -31,13 +38,15 @@ def main():
 	data_blob_shape = list(data_blob_shape)
 	net.blobs['data'].reshape(len(images), data_blob_shape[1], data_blob_shape[2], data_blob_shape[3])
 
+
+
 	# print net.blobs['data'].data.shape
-	images_with_path = [DATA_PATH + i + '.jpg' for i in images]
 	print images_with_path
 	net.blobs['data'].data[...] = map(lambda x: transformer.preprocess('data',caffe.io.load_image(x)), images_with_path)
-
+	# print net.blobs.keys()
+	# net.blobs['label'] = labels[:, 0:2]
 	# net.step(2)
-	solver.step(1)
+	# solver.step(1)
 
 	out = net.forward()
 	print out
