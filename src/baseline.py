4ebc60f02a175a865b68ae41986f13a2fc835cb2
Fahim Imaduddin Dalvi <dalvifahim@gmail.com>
2016-03-01 03:30:08 -0800

    Backward pass working! Overfitting status: looks good

--- src/baseline.py
+++ src/baseline.py
@@ -5,53 +5,49 @@ import os
 import util
 
 DATA_PATH = '../data/dev_set/images_cropped/'
+WEIGHTS_PATH = 'vgg_face_caffe/VGG_FACE.caffemodel'
+ARCH_PATH = 'DeepFaceNet.prototxt'
+SOLVER_PATH = 'solver.prototxt'
 
-
-def main():
+def evaluate():
 	caffe.set_mode_gpu()
+	assert os.path.exists(WEIGHTS_PATH)
+	assert os.path.exists(ARCH_PATH)
 
-	images = ['Barack_Obama_441', 'Ali_Landry_78', 'Ben_Stiller_69']
-	images_with_path = [DATA_PATH + i + '.jpg' for i in images]
-
-	labels = util.get_attributes('../data/pubfig_attributes.txt', images)
-	print labels.shape
-
-	weights_path = 'vgg_face_caffe/VGG_FACE.caffemodel'
-	assert os.path.exists(weights_path)
-
-	solver_path = 'DeepFaceNet.prototxt'
-	assert os.path.exists(solver_path)
-
-
-	# caffe.set_mode_cpu()
-	net = caffe.Net(solver_path,
-	                weights_path,
+	net = caffe.Net(ARCH_PATH,
+	                WEIGHTS_PATH,
 	                caffe.TEST)
-	# solver = caffe.SGDSolver('solver.prototxt')
-	# solver.net.copy_from(weights_path)
-
-	transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
-	transformer.set_transpose('data', (2,0,1))
-
-	# Reshape to match batch size
-	data_blob_shape = net.blobs['data'].data.shape
-	data_blob_shape = list(data_blob_shape)
-	net.blobs['data'].reshape(len(images), data_blob_shape[1], data_blob_shape[2], data_blob_shape[3])
-
-
-
-	# print net.blobs['data'].data.shape
-	print images_with_path
-	net.blobs['data'].data[...] = map(lambda x: transformer.preprocess('data',caffe.io.load_image(x)), images_with_path)
-	# print net.blobs.keys()
-	# net.blobs['label'] = labels[:, 0:2]
-	# net.step(2)
-	# solver.step(1)
 
 	out = net.forward()
-	print out
-	# print("Predicted class is #{}.".format(out['prob'][0].argmax()))
+	# Old head
+	# print net.blobs['prob'].data
+	# print out['argmax']
+	# 
+	# Old head without ARGMAX
+	# for i in xrange(len(out['prob'])):
+		# print("Predicted class is #{}.".format(out['prob'][i].argmax()))
+	
+	print out['loss-1']
+	print out['loss-2']
+	# print out['argmax-1']
+	# print out['argmax-2']
+	# print out['label-1']
+	# print out['label-2']
+	print "done."
 
+def train():
+	if not os.path.exists('snapshots'):
+		os.makedirs('snapshots')
+	caffe.set_mode_gpu()
+	solver = caffe.get_solver(SOLVER_PATH)
+	print "Loading old weights..."
+	solver.net.copy_from(WEIGHTS_PATH)
+	print "Stepping..."
+	solver.solve()
+	print "done."
+
+def main():
+	train()
 
 if __name__ == '__main__':
 	main()
