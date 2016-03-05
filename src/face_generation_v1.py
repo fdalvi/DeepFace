5e5bdf0508fb3a21bb45be1f3d7ddefb1e8f27b7
Fahim Imaduddin Dalvi <dalvifahim@gmail.com>
2016-03-05 15:09:49 -0800

    Finally generating creepy images

--- src/face_generation_v1.py
+++ src/face_generation_v1.py
@@ -4,11 +4,21 @@ import random
 import caffe
 import matplotlib.pyplot as plt
 
+def deprocess_image(X, mean_image):
+	r = X.copy()
+	r = r.astype(np.int)
+	r += mean_image
+	return r
+
 def class_visualization(target_y): 
 	L2_REG = 1e-6
-	learning_rate = 10000
+	LEARNING_RATE = 20000
+	NUM_ITERATIONS = 200
+	MAX_JITTER = 4
+
 	solver_path = './DeepFaceNetDeploy.prototxt'
 	weights_path = './snapshots/_iter_20000.caffemodel'
+	mean_image = np.load("../data/mean_image.npy").astype(np.int)
 
 	# Load the network
 	net = caffe.Net(solver_path, 
@@ -16,7 +26,8 @@ def class_visualization(target_y):
 					caffe.TRAIN)
 
 	# Start with a random image
-	X = np.random.randn(224,224,3)
+	X = np.random.randint(0, 256, size=(224,224,3)).astype(np.float)
+	X -= mean_image
 
 	# Set up blob data
 	transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
@@ -33,56 +44,56 @@ def class_visualization(target_y):
 		diff = net.blobs['fc8-%d'%(i)].diff
 		one_hot = np.zeros_like(diff)
 		one_hot[0, target_y[i-1]] = 1
-		# net.blobs['fc8-%d'%(i)].data[...] = one_hot
+
 		net.blobs['fc8-%d'%(i)].diff[...] = one_hot
+
 		one_hots.append(one_hot)
 
-	for t in xrange(10):
-	# 	# As a regularizer, add random jitter to the image
-	# 	ox, oy = np.random.randint(-max_jitter, max_jitter+1, 2)
-	# 	X = np.roll(np.roll(X, ox, -1), oy, -2)
-		print 'Performing forward pass...'
+	# print 'Before'
+	# print net.blobs['fc8-1'].diff
+	# print net.blobs['fc8-42'].diff
+	# _ = net.forward()
+	# dX = net.backward(start='fc8-42')
+	# print 'After'
+	# print net.blobs['fc8-1'].diff
+	# print net.blobs['fc8-42'].diff
+
+	# print mean_image.flatten()[0:10]
+	for t in xrange(NUM_ITERATIONS):
+		# As a regularizer, add random jitter to the image
+		ox, oy = np.random.randint(-MAX_JITTER, MAX_JITTER+1, 2)
+		X = np.roll(np.roll(X, ox, -1), oy, -2)
+
+		print 'Performing iteration %d...'%(t+1)
 		net.blobs['data'].data[...] = transformer.preprocess('data', X)
 		for i in xrange(1,43):
 			net.blobs['fc8-%d'%(i)].diff[...] = one_hots[i-1]
 
 		_ = net.forward()
-		dX = net.backward()
+		dX = net.backward(start='fc8-42')
 		dX = dX['data']
 		dX = dX[0, :, :, :]
 		dX = np.transpose(dX, (1, 2, 0))
 
 		dX -= 2*L2_REG*X
-		X += learning_rate*dX
+		X += LEARNING_RATE*dX
 
-		print 'done...'
-		# deprocess_image(X, data['mean_image'])
-		plt.imshow(X)
-		# plt.gcf().set_size_inches(3, 3)
-		plt.axis('off')
-		plt.savefig('outputs/image-%d.png'%(t))
- #  
-	# 	############################################################################
-	# 	#                             END OF YOUR CODE                             #
-	# 	############################################################################
+		# Undo the jitter
+		X = np.roll(np.roll(X, -ox, -1), -oy, -2)
 		
-	# 	# Undo the jitter
-	# 	X = np.roll(np.roll(X, -ox, -1), -oy, -2)
+		# As a regularizer, clip the image
+		X = np.clip(X, -mean_image, 255.0 - mean_image)
 		
-	# 	# As a regularizer, clip the image
-	# 	X = np.clip(X, -data['mean_image'], 255.0 - data['mean_image'])
+		# As a regularizer, periodically blur the image
+		# if t % blur_every == 0:
+		# 	X = blur_image(X)
 		
-	# 	# As a regularizer, periodically blur the image
-	# 	if t % blur_every == 0:
-	# 		X = blur_image(X)
-		
-	# 	# Periodically show the image
-	# 	if t % show_every == 0:
-	# 		plt.imshow(deprocess_image(X, data['mean_image']))
-	# 		plt.gcf().set_size_inches(3, 3)
-	# 		plt.axis('off')
-	# 		plt.show()
- #  return X
+		if t % 10 == 0:
+			print 'Saving image %d'%(t)
+			plt.clf()
+			plt.imshow(deprocess_image(X, mean_image))
+			plt.axis('off')
+			plt.savefig('outputs/image-%d.png'%(t))
 
 def main(): 
 	target_y = np.array([0]*42)
